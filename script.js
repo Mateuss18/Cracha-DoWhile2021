480d2953c306c75da7bce786251e864bc50b107b
Mateuss18 <mateusdfl18@gmail.com>
2021-10-22 00:02:06 -0300

    Stage 3

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-  
+const linksSocialMedia = { 
+  github: "Mateuss18", 
+  youtube: "MisterMateus18", 
+  facebook: "mateus.antonio.10888",
+  instagram: "mateuss_18", 
+  twitter: "Mateuss189"
+}
+
+function changeSocialMediaLinks(){
+  for(let li of socialLinks.children){
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
